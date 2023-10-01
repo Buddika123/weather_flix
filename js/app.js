962a1772d04f826c6c999b69a85cc2079f13fc7d
Buddika <kumaraoabd@gmail.com>
2023-10-02 01:10:07 +0530

    current weather

--- js/app.js
+++ js/app.js
@@ -1,25 +1,9 @@
-// function getweather(){
-//     console.log("clicked")
-  
-//     //fjghfjg
-//     fetch("http://api.weatherapi.com/v1/current.json?key=bfbf2b8896d94de188590948230110&q=colombo&aqi=no", {
-//   "method": "GET",
-//   "headers": {
-//   }
-//   })
-// .then(response => {
-//   console.log(response.body);
-// })
-// .catch(err => {
-//     console.log("There was an error")
-//   console.error(err);
-// });
-// }
 
 function getWeather() {
     // Get the user's input (e.g., city or ZIP code)
+    const inputLocation = document.getElementById("location").value;
     
-   const location = document.getElementById("location").value || "Colombo";
+   const location = `${inputLocation} == null? "Colombo" : ${inputLocation}`;
 
     // API key
    const apiKey = "bfbf2b8896d94de188590948230110"; 
@@ -41,27 +25,37 @@ function getWeather() {
             // Access the weather information from the parsed JSON
             const temperature = data.current.temp_c;
             const condition = data.current.condition.text;
-            const humidity = data.current.humidity;
+            //const humidity = data.current.humidity;
 
             console.log(temperature)
             console.log(data)
             
             // Update the HTML to display the weather information
-            // const weatherInfo = `
+          
               const city = `<h1> ${data.location.name}</h1>
-              <img src=\"${data.current.condition.icon}\" >`;
-              const image = `<img src="${data.current.condition.icon}" width=\"200px\" height=\"150px\">`
-            //   el.innerHTML="<img src=\"http://placehold.it/350x350\" width=\"400px\" height=\"150px\">";
-            //     <img src="data.current.condition.icon">
-            //     <p> ${temperature}°C</p>
-            //     <p> ${condition}</p>
-               
-            //     <p>Humidity: ${humidity}%</p>
-            // `;
+                            <img src=\"${data.current.condition.icon}\"  width=\"200px\" height=\"150px\">
+                            <p>${data.current.condition.text}</p>`;
+             
+              const temp = `<h1> Temperature</h1>
+                            <p>${temperature}°C</>`
+              const humidity = `<h1> Humidity</h1>
+                            <p>${data.current.humidity}%</>`
+
+              const cloud =  `<h1> Cloud</h1>
+                        <p>${data.current.cloud}%</>`
+
+              const wind =  `<h1> Wind</h1>
+                            <p>${data.current.wind_kph}kmPH/>`
+          
+          
 
             const localTime = `<h1>${data.location.localtime}</h1>`
             
              document.getElementById("city").innerHTML = city;
+             document.getElementById("temp").innerHTML = temp;
+             document.getElementById("hum").innerHTML = humidity;
+             document.getElementById("cloud").innerHTML = cloud;
+             document.getElementById("wind").innerHTML = wind;
              document.getElementById("banner").innerHTML = localTime;
         })
         .catch(error => {
